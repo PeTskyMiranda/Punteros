ee395aa43b1d81dd8681fc35263dc96df95d951b
PeTskyMiranda <pepemiraanda@gmail.com>
2019-08-30 12:16:39 -0600

    Nuevo puntero Mejorado

--- puntero.cpp
+++ puntero.cpp
@@ -1,20 +1,43 @@
-#include <iostream>
-#include <conio.h>
+#include <bits/stdc++.h>
+
 using namespace std;
 
+struct lugar{
+    char habitad[20];
+    char ciudad[20];
+};
+struct animal{
+    char nombre[20];
+    int edad;
+    float estatura;
+    struct lugar lugr;
+};
+
 int main(int argc, char** argv){
+    struct animal P1[5];
+    struct animal *apuntador;
 
-    int arreglo[10];
-    int mayor=INT_MIN;
-    int *puntero=arreglo;
+    apuntador=&P1[0];
 
-    for (;puntero<=&arreglo[9];puntero++){
-        cout<<"Ingresa un valor: "<<endl;
-        cin>>*puntero;
-        if(*puntero>mayor) mayor=*puntero;
+    for (int i=0; i<2;i++){
+       cout<<"Nombre: ";
+       cin.getline(P1[i].nombre,20,'\n');
+       cout<<"Edad: ";
+       cin>>P1[i].edad;
+       cout<<"Estatura: ";
+       cin>>P1[i].estatura;
+       cout<<"habitad: ";
+       fflush(stdin);
+       cin.getline(P1[i].lugr.habitad,20,'\n');
+       cout<<endl;
+    }
 
-     }
-    cout<<"El valor mas grande:"<<mayor<<endl;
+
+    cout<<" Nombre guardado: "<<P1[0].nombre<<endl;
+    cout<<" Edad guardada: "<<P1[0].edad<<endl;
+    cout<<" Estarura guardada: "<<P1[0].estatura<<endl;
+    cout<<" habitad guardada: "<<P1[0].lugr.habitad<<endl;
+
+    system("pause");
     return 0;
 }
-
